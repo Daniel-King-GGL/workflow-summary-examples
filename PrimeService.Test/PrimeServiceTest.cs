e778bf2cd17e83d7189ccd5fa6b892567d395f14
Daniel King <daniel.king@gallagher.com>
2024-12-04 12:39:19 +1300

    first starter workflow

---
+++ PrimeService.Test/PrimeServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Prime.Services;
+
+namespace Prime.Test;
+
+[TestClass]
+public sealed class PrimeServiceTest
+{
+    [TestMethod]
+    public void TestIsPrime_InputIs1_ReturnFalse()
+    {
+        var primeService = new PrimeService();
+        bool result = primeService.IsPrime(1);
+
+        Assert.IsFalse(result, "1 should not be prime");
+    }
+
+    [TestMethod]
+    public void TestIsPrime_InputIs2_ReturnFalse()
+    {
+        var primeService = new PrimeService();
+        bool result = primeService.IsPrime(2);
+
+        Assert.IsFalse(result, "2 should not be prime");
+    }
+}
